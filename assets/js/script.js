7722b4c8a67cede448a62aed2a15b05b0d8b780a
Stephanie Giel <stephaniegiel@gmail.com>
2020-12-27 14:49:27 -0700

    user prompts for characters to include

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// User chosen length of password
 
+ // password initialized as an empty string with a length of 0
+//password = '';
+//passwordLength = 0;
 
 // user prompt for password length
 var passwordLengthPrmpt = function () {
   passwordLength = Number(window.prompt("Please enter a number between 8 and 128 to indicate your desired password length."));
   console.log("User chose a password length of " + passwordLength);
-    // checks to make sure password is a number between 8 and 128
+
+    // checks to make sure password is a number between 8 and 128 and if not, prompts again
     if (isNaN(passwordLength)) {
     window.alert ("You have not chosen a number. Please try again.");
     return passwordLengthPrmpt();
@@ -23,58 +26,77 @@ var passwordLengthPrmpt = function () {
   return passwordLength;
 };
 
-// function to create array from designated UTF-8 unicode characters
-var grabCharacters = function(firstChar,lastChar) {
 
-  var alphaGrab = [];
+// function to create string from designated UTF-8 unicode characters
+var getCharacters = function(firstChar,lastChar) {
+
+  var alphaGet = [];
   var start = firstChar.charCodeAt(0);
   var last = lastChar.charCodeAt(0);
   for (var i = start; i <= last; i++) {
-    alphaGrab.push(String.fromCharCode(i)); 
+    alphaGet.push(String.fromCharCode(i)); 
   }
 
-  return alphaGrab.join('');
-}; // end of grabCharacters function
+  return alphaGet.join('');
+}; // end of getCharacters function
+// helpful code found at https://gist.github.com/mreigen/fdeafcc08a9e44d976bd6a8db468c496
 
-// user prompt for character set to include
+
+// user prompted whether they would like to include any characters from a defined character set in password
 var yesOrNoPrmpt = function(characterSet) {
 
-  var yOrN;
-  // initially assumes a yes to include the character set
-  var includeChar = true;
+  // includeChar is yes if characters are to be included
+  var includeChar;
 
-  YorN = window.prompt("Please indicate with a Y/N whether you would like to include " + characterSet + " in your password.");
-  console.log(typeof YorN + " " + YorN.toLowerCase());
+  var yOrN = window.prompt("Please indicate with a Y/N whether you would like to include " + characterSet + " in your password.");
+  //console.log(typeof yOrN + " " + yOrN.toLowerCase());
   
-  if (YorN.toLowerCase() === 'y') {
+  // checks if user entered a valid response, and if not, prompts again
+  if (yOrN.toLowerCase() === 'y') {
     includeChar = true;
-  } else if (YorN.toLowerCase() === 'n') {
+  } else if (yOrN.toLowerCase() === 'n') {
     includeChar = false;
   } else {
     window.alert("You have entered an incorrect choice.");
     return yesOrNoPrmpt(characterSet);
   }
+  //console.log("IncludeChar " + includeChar);
   return includeChar;
 }; // end of yesOrNoPrompt function
 
+
+var getRandomNumber = function(min,max) {
+  var num = Math.floor(Math.random()*(max - min +1)) + min;
+  return num;
+};
+
+
 // generates the Password based on criteria provided by user
 var generatePassword = function () {
-  var passwordLength = 0;
-  var specialChars = " !\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~";
+
+  // password initialized as an empty string with a length of 0
+  passwordArray = [];
+
+  var specialChars = "!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~";
   // console.log(specialChars + " is a " + typeof specialChars + " length " + specialChars.length);
-  var alphaUpperCase = grabCharacters("A","Z");
+  var alphaUpperCase = getCharacters("A","Z");
   // console.log(alphaUpperCase + " is a " + typeof alphaUpperCase + " length " + alphaUpperCase.length);
-  var alphaLowerCase = grabCharacters("a","z");
-  // console.log(alphaLowerCase + " is a " + typeof alphaLowerCase + " length " + alphaLowerCase.length);
-  var numbers = grabCharacters("0","9");
-  // console.log(numbers + " is a " + typeof numbers + " length " + numbers.length);
+  var alphaLowerCase = getCharacters("a","z");
+  console.log(alphaLowerCase + " is a " + typeof alphaLowerCase + " length " + alphaLowerCase.length);
+  var numbers = getCharacters("0","9");
+  console.log(numbers + " is a " + typeof numbers + " length " + numbers.length);
+  
+  // do not include characters unless user choses - so initialize to false
   var includeLowerCase = false;
   var includeUpperCase = false
   var includeSpecialChars = false;
   var includeNumbers = false;
   var includeAtleastOne = false;
 
-  // prompts for password length
+  var charLocation = -1;
+  var characterString = '';
+
+  // prompts user for password length
   passwordLengthPrmpt();
 
   // prompts users for what type of characters to include, checking that at least one is chosen
@@ -82,12 +104,16 @@ var generatePassword = function () {
 
     // prompt user if they would like to include lower case letters in password
     includeLowerCase = yesOrNoPrmpt("lower case letters a to z");
+    console.log("Include lower case = " + includeLowerCase);
     // prompt user if they would like to include upper case letters in password
     includeUpperCase = yesOrNoPrmpt("upper case letters A to Z");
+    console.log("Include upper case = " + includeUpperCase);
     // prompt user if they would like to include numbers in password
     includeNumbers = yesOrNoPrmpt("numbers 0 to 9");
+    console.log("Include numbers = " + includeNumbers);
     // prompt user if the would like to include special characters in password
     includeSpecialChars = yesOrNoPrmpt("special characters");
+    console.log("Include special characters = " + includeSpecialChars);
 
     // check if at least one type of character was chosen, and if not, prompt user again
     if (includeLowerCase === false && includeUpperCase === false && includeNumbers === false &&
@@ -97,30 +123,77 @@ var generatePassword = function () {
     } else {
       includeAtleastOne = true;
     }
+
+    console.log("includeAtleastOne = " + includeAtleastOne);
+
+    if (includeLowerCase === true) {
+      characterString += alphaLowerCase;
+    }
+
+    if (includeUpperCase === true) {
+      characterString += alphaUpperCase;
+    } 
+
+    if (includeNumbers === true) {
+      characterString += numbers;
+    } 
+
+    if (includeSpecialChars === true) {
+      characterString += specialChars;
+    }
+    
   } // end of while !includeAtleastOne
 
-  console.log("user wants to include lower case = " + includeLowerCase);
-  console.log("user wants to include upper case = " + includeUpperCase);
-  console.log("user wants to include numbers = " + includeNumbers);
-  console.log("user wants to include special characters = " + includeSpecialChars);
+  console.log("final characterString = " + characterString);
+
+  if (includeLowerCase) {
+    charLocation = getRandomNumber(0,alphaLowerCase.length-1);
+    passwordArray.push(alphaLowerCase.charAt(charLocation));
+    console.log("password 1st item = " + passwordArray + " using a random location of " + charLocation);
+  }
+
+  if (includeUpperCase) {
+    charLocation = getRandomNumber(0,alphaUpperCase.length-1);
+    //passwordBuild = passwordBuild.concat(alphaUpperCase.charAt(charLocation));
+    passwordArray.push(alphaUpperCase.charAt(charLocation));
+    console.log("password 1st item = " + passwordArray + " using a random location of " + charLocation);
+  }
+
+  if (includeNumbers) {
+    charLocation = getRandomNumber(0,numbers.length-1);
+    //passwordBuild = passwordBuild.concat(numbers.charAt(charLocation));
+    passwordArray.push(numbers.charAt(charLocation));
+    console.log("password 1st item = " + passwordArray + " using a random location of " + charLocation);
+  }
+
+  if (includeSpecialChars) {
+    charLocation = getRandomNumber(0,specialChars.length-1);
+    //passwordBuild = passwordBuild.concat(specialChars.charAt(charLocation));
+    passwordArray.push(specialChars.charAt(charLocation));
+    console.log("password 1st item = " + passwordArray + " using a random location of " + charLocation);
+  }
+
+
+  //console.log("user wants to include lower case = " + includeLowerCase);
+  //console.log("user wants to include upper case = " + includeUpperCase);
+  //console.log("user wants to include numbers = " + includeNumbers);
+  //console.log("user wants to include special characters = " + includeSpecialChars);
+
+  // makes sure to randomly grab at least one character from each character set user requested
 
   
-
 }; // end of generatePassword function
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   
-
-
-
   password = 'abcdefghijk';
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
